893e5688428512582d9a9bb505333f8106c21440
artur.usmanov <artur.usmanov@x5.ru>
2021-12-27 19:21:57 +0300

    Реализация кэша

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,57 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+func (lC *lruCache) Set(key Key, value interface{}) bool {
+	// формируем элемент кеша
+	cItem := &cacheItem{key, value}
+
+	// пытаемся получить элемент из словаря по ключу
+	mapItem, ok := lC.items[key]
+
+	if ok {
+		// элемент присутствует в словаре, меняем значение и перемещаем в начало очереди
+		mapItem.Value = cItem
+		lC.queue.MoveToFront(mapItem)
+	} else {
+		// элемент отсутствует
+		// проверка переполнения кеша
+		if lC.queue.Len() >= lC.capacity {
+			// не хватает места для нового элемента
+			// получаем последний элемент очереди
+			lastItem := lC.queue.Back()
+			// удаляем его из очереди
+			lC.queue.Remove(lastItem)
+			// и из словаря
+			delete(lC.items, lastItem.Value.(*cacheItem).key)
+		}
+
+		// добавляем в очередь и в словарь
+		lC.items[key] = lC.queue.PushFront(cItem)
+	}
+	return ok
+}
+
+func (lC *lruCache) Get(key Key) (interface{}, bool) {
+	// пытаемся получить элемент из словаря по ключу
+	if mapItem, ok := lC.items[key]; ok {
+		// элемент присутствует в словаре, перемещаем в начало очереди
+		lC.queue.MoveToFront(mapItem)
+		// и возвращаем
+		return mapItem.Value.(*cacheItem).value, true
+	} else {
+		// элемент отсутствует
+		return nil, false
+	}
+
+}
+
+func (lC *lruCache) Clear() {
+
 }
 
 type cacheItem struct {
