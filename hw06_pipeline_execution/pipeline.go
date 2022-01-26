8e9335f48a199eda8addd791138e85f2aec39f8e
artur.usmanov <artur.usmanov@x5.ru>
2022-01-26 22:09:15 +0300

    ExecutePipeline

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,36 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+	for _, stage := range stages {
+		out = stage(checkStage(done, out))
+	}
+
+	return out
+}
+
+func checkStage(done In, in In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+
+		for {
+			select {
+			case value, ok := <-in:
+				if ok {
+					// put to out chan
+					out <- value
+				} else {
+					// empty chan
+					return
+				}
+			case <-done:
+				// terminate
+				return
+			}
+		}
+	}()
+
+	return out
 }
