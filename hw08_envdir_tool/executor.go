ab19ca1f2b690f9f2ea739c399d6374af775d15a
artur.usmanov <artur.usmanov@x5.ru>
2022-02-09 23:16:40 +0300

    HW8 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,53 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	// проверка наличия команд и аргументов
+	if len(cmd) == 0 {
+		return 0
+	}
+
+	// парсим cmd
+	command := cmd[0]
+	args := cmd[1:]
+
+	// получаем структуру для выполнения команды
+	com := exec.Command(command, args...)
+	// пробрасываем потоки ввода/вывода и ошибок
+	com.Stdout = os.Stdout
+	com.Stderr = os.Stderr
+	com.Stdin = os.Stdin
+
+	// берем окружение
+	comEnv := os.Environ()
+	for fileName, v := range env {
+		// сверяем параметры
+		for i, str := range comEnv {
+			// если параметр совпадает с именем файла
+			if strings.Split(str, "=")[0] == fileName {
+				// удаляем
+				comEnv = append(comEnv[:i], comEnv[i+1:]...)
+			}
+		}
+		if !v.NeedRemove {
+			// если не помечен на удаление, то добавляем в конец
+			comEnv = append(comEnv, fmt.Sprintf("%s=%s", fileName, v.Value))
+		}
+	}
+	// присваиваем окружение команде
+	com.Env = comEnv
+
+	// запускаем
+	if err := com.Run(); err != nil {
+		return 1
+	}
+
+	return 0
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		log.Fatal("Wrong arguments count")
+		// os.Exit(1)
+	}
+
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		log.Fatalf("Failed read %s: %s", os.Args[1], err.Error())
+		// os.Exit(1)
+	}
+
+	code := RunCmd(os.Args[2:], env)
+
+	os.Exit(code)
 }
